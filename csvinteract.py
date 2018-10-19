d75c540cef3e898fc6fa6212211cbce18718ff4b
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-10-19 23:35:50 +0200

    Support floats. Now take str as argument.
    Tests improved.

--- csvinteract.py
+++ csvinteract.py
@@ -1,9 +1,32 @@
 import csv
 
 
+class ConversionError(Exception):
+    pass
+
+
+def isnumeric(x: str) -> bool:
+    return x.replace('.', '', 1).isdigit()
+
+
+def parse(content: str):
+    if isnumeric(content):
+        try:
+            content = float(content)
+        except TypeError as e:
+            raise ConversionError from e
+        print(content)
+        if int(content) == content:
+            return content
+        return content
+    else:
+        return content
+
+
 def extract(file):
     res = []
-    reader = csv.reader(file, delimiter=",", quotechar='"')
-    for row in reader:
-        res.append(row)
-    return res
+    with open(file, 'r') as opened_file:
+        reader = csv.reader(opened_file, delimiter=",", quotechar='"')
+        for row in reader:
+            res.append(row)
+    return [[parse(cell) for cell in row] for row in res]
--- tests/tests_csvinteract.py
+++ tests/tests_csvinteract.py
@@ -1,11 +1,36 @@
 import unittest
+from unittest import mock
 
 import csvinteract
 
 
 class CSVExtractTest(unittest.TestCase):
 
-    def test_with_just_some_text(self):
-        csv_file = open("./base_test.csv", "r")
-        csv_extracted = csvinteract.extract(csv_file)
-        self.assertEqual(csv_extracted, [['a', 'b'], ['c', 'd']])
+    @mock.patch("csvinteract.open")
+    def meta_test(self, value_called, expected_value, mock_open):
+        # Preparation
+        mock_open.return_value = mock_open
+        mock_open.__enter__.return_value = value_called
+        file = mock.sentinel.file_name
+
+        # Execution
+        csv_extracted = csvinteract.extract(file)
+
+        # Analyse du résultat
+        mock_open.assert_called_with(mock.sentinel.file_name, 'r')
+        self.assertEqual(csv_extracted, expected_value)
+
+    def test_with_text(self):
+        expected_value = [['a', 'b'], ['c', 'd']]
+        value_called = [','.join(row) for row in expected_value]
+        self.meta_test(value_called, expected_value)
+
+    def test_with_text_and_int(self):
+        expected_value = [[0, 'b'], [1, 'd']]
+        value_called = ["0,b", "1,d"]
+        self.meta_test(value_called, expected_value)
+
+    def test_with_float(self):
+        expected_value = [[0.5, 'b'], [1, 'd']]
+        value_called = ["0.5,b", "1,d"]
+        self.meta_test(value_called, expected_value)
