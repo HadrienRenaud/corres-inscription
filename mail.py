e6ba158b9b4144478dc43c503e3199d77f3fa8c9
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-10-20 15:04:21 +0200

    Coding Mail.
    Signed-off-by: Hadrien RENAUD <hadrien.renaud@polytechnique.edu>

--- mail.py
+++ mail.py
@@ -1,3 +1,50 @@
 import smtplib
+from email.message import EmailMessage
+from typing import Union, List, TypeVar
 
-smtp = smtplib.SMTP("myconfig", "myport", "localhostname")
+SMTP_SERVER = "smtp.gmail.com:587"
+SMTP_USER = "hadrien.renaud@animath.fr"
+SMTP_PASSWORD = "m57kIlxhhqNu"
+
+
+class SendingError(Exception):
+    pass
+
+
+T = TypeVar('T')
+OneOrMany = Union[T, List[T]]
+
+
+def send(content: str, sender: OneOrMany[str], receivers: OneOrMany[str],
+         subject: str = None, **kwargs):
+    """Send message."""
+    # Message setup
+    msg = EmailMessage()
+    msg.set_content(content)
+    # msg["From"] = _format_mail(sender)
+    # msg['To'] = _format_mail(receivers)
+    msg["From"] = sender
+    msg['To'] = receivers
+
+    if subject is not None:
+        msg["Subject"] = subject
+
+    for key, val in kwargs.items():
+        msg[key] = val
+
+    # Sending message
+    with smtplib.SMTP(SMTP_SERVER) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.send_message(msg)
+
+
+if __name__ == '__main__':
+    message = """Ceci est un message de test ! 
+        Le script a l'air de fonctionner."""
+    source = "hadrien.renaud@animath.fr"
+    target = "hadrien.renaud@animath.fr"
+    subject = "Test du script d'envoi de mail"
+
+    send(message, source, target, subject=subject)
--- tests/tests_mail.py
+++ tests/tests_mail.py
@@ -1,10 +1,33 @@
 import unittest
 from unittest import mock
 
+import mail
+
+
+def identity(args):
+    """Identity function x -> x."""
+    return args
+
 
 class SendMailTests(unittest.TestCase):
 
+    @mock.patch("mail.EmailMessage")
     @mock.patch("mail.smtplib.SMTP")
-    def basic_test_case(self, mock_smtp):
+    def test_basic(self, mock_smtp, mock_email_message):
         # Mock configuration
-        pass
+        mock_smtp.__enter__.return_value = mock_smtp
+        mock_msg = mock.MagicMock()
+        mock_email_message.return_value = mock_msg
+
+        # Execution
+        mail.send(mock.sentinel.content, mock.sentinel.sender,
+                  mock.sentinel.receiver)
+
+        # Verification
+        mock_smtp.return_value = mock_smtp
+        # mock_smtp.__enter__.assert_called_once()
+        mock_smtp.assert_called_once()
+        mock_email_message.assert_called_once()
+        mock_msg.set_content.assert_called_with(mock.sentinel.content)
+        mock_msg.__setitem__.assert_any_call("To", mock.sentinel.receiver)
+        mock_msg.__setitem__.assert_any_call("From", mock.sentinel.sender)
